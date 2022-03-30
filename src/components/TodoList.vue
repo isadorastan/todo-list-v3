2472dd743ee7bdda22da1e7cb1b4a8711254d8c4
Isadora Stangarlin <isadorastangarlin@MacBook-Air-de-Isadora.local>
2022-03-30 16:54:07 -0300

    Todo list simples

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,26 @@
 <template>
-  Todo
+  <input type="text" @change="addToList" v-model="text">
+  <ul>
+      <li v-for="(item, index) in list" :key="index">
+          {{item}}
+      </li>
+  </ul>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            list: [],
+            text: ''
+        }
+    },
+    methods: {
+        addToList() {
+            this.list.unshift(this.text);
+            this.text = '';
+        }
+    }
 }
 </script>
 
